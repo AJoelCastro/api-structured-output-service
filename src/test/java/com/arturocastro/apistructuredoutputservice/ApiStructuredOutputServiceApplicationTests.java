9aa6489b4559ef022513fda2817b4ebf14b2efdf
AJoelCastro <misthorn33@gmail.com>
2025-10-26 22:10:10 -0500

    first commit

---
+++ src/test/java/com/arturocastro/apistructuredoutputservice/ApiStructuredOutputServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturocastro.apistructuredoutputservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStructuredOutputServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
